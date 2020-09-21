8d22bb94719e0dc0cc819ea67ae2ab097160ed0f
ConnorChan <connorchanchuang@gmail.com>
2020-09-21 09:38:47 +0800

    Java 100-1

---
+++ learn/src/com/tvg/java1/Ch_1.java
@@ -0,0 +1,26 @@
+package com.tvg.java1;
+
+import java.util.Scanner;
+
+public class Ch_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,j,k,n;
+		Scanner input = new Scanner(System.in);
+		System.out.println("请输入金字塔层数:");
+		n = input.nextInt();
+		for (i = 1; i < n; i++) {
+			for (j=1; j<=n-i;j++) {
+				System.out.print(" ");
+			}				
+			for (k=1;k<=2*i-1;k++) {
+				System.out.print("*");
+			}	
+			System.out.print("\n");
+		}
+		input.close();
+
+	}
+
+}
